cd23c3ebbda7c476dfe6a9270a2449763b3745d9
akbar <akbar.ali@mobinius.com>
2019-12-16 17:48:27 +0530

    6.6: anecdotes, step4 6.7: anecdotes, step5 6.8: anecdotes, step6

--- src/App.js
+++ src/App.js
@@ -1,37 +1,15 @@
 import React from 'react';
-import {vote, createNew} from './reducers/anecdoteReducer'
+import {vote, createNew} from './reducers/anecdoteReducer';
+import AnecdoteForm from './components/Anecdoteform';
+import AnecdoteList from './components/Anecdotelist';
 const App = (props) => {
-  const anecdotes = props.store.getState().sort((a, b)=> {if (a.votes>b.votes) {return -1} else {return 1} })
-
-  const _vote = (id) => {
-    props.store.dispatch(vote(id))
-  }
-    const onSubmit = (e) => {
-        e.preventDefault();
-        props.store.dispatch(createNew(e.target.content.value.trim()))
-
-    }
-  return (
-    <div>
-      <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote =>
-        <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => _vote(anecdote.id)}>vote</button>
-          </div>
+    return (
+        <div>
+            <h2>Anecdotes</h2>
+            <AnecdoteForm store={props.store} />
+            <AnecdoteList store={props.store} />
         </div>
-      )}
-      <h2>create new</h2>
-      <form onSubmit={onSubmit}>
-        <div><input name="content"/></div>
-        <button>create</button>
-      </form>
-    </div>
-  )
+    )
 }
 
 export default App
